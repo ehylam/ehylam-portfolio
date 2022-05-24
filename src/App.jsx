cf8429804b0a1cd018ac1ff8163356869648ed87
ehylam <48621527+ehylam@users.noreply.github.com>
2022-05-24 17:46:45 +1000

    reverted JS filename in the utils folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import './App.css';
 import Hero from './components/Hero';
 import Image from './components/Image';
-import ImageTransition from './lib/imageTransition';
+import ImageTransition from './lib/ImageTransition';
 import Footer from './components/Footer';
 
 
